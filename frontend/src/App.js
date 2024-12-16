bfe49f6710e2941377960b8fb09cf977289e826b
“Erik <eweinmeister@unomaha.edu>
2024-12-16 17:38:42 -0600

    pushing what I have here

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+
 import ComicList from './components/ComicList';
 import ComicForm from './components/ComicForm';
-import HomePage from './components/HomePage';
+//import HomePage from './components/HomePage';
 
+//this is where it puts the sections on the page
 function App() {
   return (
     <div>
       <h1>Comic Inventory</h1>
+
       <ComicForm />
-      <br></br>
+
       <ComicList />
     </div>
   );
